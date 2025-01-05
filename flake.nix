0cae669e812f991a6a064617887139b5bf0301f5
Denis Keksel <Denis Keksel>
2025-01-06 03:03:19 +0400

    init flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,52 @@
 {
-  description = "A very basic flake";
+  description = "Denis Keksel's Neovim configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "unstable";
+    };
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      src = ./.;
+      snowfall = {
+        namespace = "flocke";
+      };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      channels-config.allowUnfree = true;
+      alias.packages.default = "neovim";
+      overlays = with inputs; [nixvim.overlays.default];
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      outputs-builder = channels: {
+        formatter = channels.nixpkgs.nixfmt-rfc-style;
+        checks.pre-commit-check = inputs.pre-commit-hooks.lib.${channels.nixpkgs.system}.run {
+          src = ./.;
+          hooks = {
+            nixfmt = {
+              enable = true;
+              entry = "${channels.nixpkgs.nixfmt-rfc-style}/bin/nixfmt";
+              extraPackages = [ channels.nixpkgs.nixfmt-rfc-style ];
+            };
+          };
+        };
 
-  };
+      };
+
+    };
 }
