334395ed1aa5b3818cb8bad1d9d8777146c6fbb6
Denis Keksel <Denis Keksel>
2025-01-12 19:24:48 +0400

    nixvim yazi setup changed

--- modules/nixvim/yazi/default.nix
+++ modules/nixvim/yazi/default.nix
@@ -1,23 +1,22 @@
 {
-  programs.nixvim = {
     plugins.yazi = {
       enable = true;
     };
 
     keymaps =   [
       {
-      mode = "n";
-      key = "<leader>e";
-      action.__raw = ''
-        function()
-        require('yazi').yazi()
-        end
-      '';
-      options = {
-        desc = "Yazi toggle";
-        silent = true;
-      };
-    }
-  ];
-  };
+        mode = "n";
+        key = "<leader>e";
+        action.__raw = ''
+          function()
+          require('yazi').yazi()
+          end
+        '';
+        options = {
+          desc = "Yazi toggle";
+          silent = true;
+        };
+      }
+    ];
+
 }
