a504254abe352d8ad1270b53e1e89a94cf8c9918
Denis Keksel <kexsel@yandex.ru>
2025-01-31 22:35:49 +0400

    qmk settings changed

--- modules/nixvim/qmk/default.nix
+++ modules/nixvim/qmk/default.nix
@@ -1,31 +1,33 @@
 {
   plugins.qmk = {
     enable = true;
-    auto_format_pattern = "*keymap.c";
-    comment_preview = {
-      keymap_overrides = { };
-      position = "top";
-      symbols = {
-        bl = "└";
-        bm = "┴";
-        br = "┘";
-        horz = "─";
-        ml = "├";
-        mm = "┼";
-        mr = "┤";
-        space = " ";
-        tl = "┌";
-        tm = "┬";
-        tr = "┐";
-        vert = "│";
+    settings = {
+      auto_format_pattern = "*keymap.c";
+      comment_preview = {
+        keymap_overrides = { };
+        position = "top";
+        symbols = {
+          bl = "└";
+          bm = "┴";
+          br = "┘";
+          horz = "─";
+          ml = "├";
+          mm = "┼";
+          mr = "┤";
+          space = " ";
+          tl = "┌";
+          tm = "┬";
+          tr = "┐";
+          vert = "│";
+        };
       };
+      layout = [
+        "x x"
+        "x^x"
+      ];
+      name = "LAYOUT_preonic_grid";
+      timeout = 5000;
+      variant = "qmk";
     };
-    layout = [
-      "x x"
-      "x^x"
-    ];
-    name = "LAYOUT_preonic_grid";
-    timeout = 5000;
-    variant = "qmk";
   };
 }
